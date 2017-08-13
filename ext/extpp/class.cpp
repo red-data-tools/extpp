a1471166e6e5ebe4771096c51637847794fa7703
Kouhei Sutou <kou@clear-code.com>
2017-08-13 22:46:57 +0900

    Use more auto

--- ext/extpp/class.cpp
+++ ext/extpp/class.cpp
@@ -4,7 +4,7 @@ namespace {
   VALUE call_func(int args, VALUE *argv, VALUE self) {
     auto method_id = rb_sym2id(rb_funcall(self, rb_intern("__method__"), 0));
     auto rb_function = rb_ivar_get(rb_obj_class(self), method_id);
-    rb::Function *function = rb::Function::from_ruby(rb_function);
+    auto function = rb::Function::from_ruby(rb_function);
     return function->call(self);
   };
 }
@@ -18,8 +18,8 @@ namespace rb {
 
   Class &Class::define_method(const char *name,
                               std::function<VALUE(VALUE)> body) {
-    Function *function = new Function(body);
-    VALUE rb_function = function->to_ruby();
+    auto function = new Function(body);
+    auto rb_function = function->to_ruby();
     rb_iv_set(class_, name, rb_function);
     rb_define_method(class_, name, reinterpret_cast<METHOD_FUNC>(call_func), -1);
     return (Class &)*this;
