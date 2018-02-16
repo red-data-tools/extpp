3f15796f5aa6f85f021ae7feca06a0a490c51b93
Kouhei Sutou <kou@clear-code.com>
2018-02-16 23:30:46 +0900

    Simplify sample

--- sample/hello/hello.cpp
+++ sample/hello/hello.cpp
@@ -6,16 +6,18 @@ void
 Init_hello(void)
 {
   rb::Class klass("Hello");
-  klass.define_method("initialize", [](VALUE self, int argc, VALUE *argv) {
+  klass.define_method("initialize", [](VALUE rb_self, int argc, VALUE *argv) {
       VALUE rb_name;
       rb_scan_args(argc, argv, "1", &rb_name);
-      rb_ivar_set(self, rb_intern("@name"), rb_name);
+      rb::Object self(rb_self);
+      self.ivar_set("@name", rb_name);
       return Qnil;
     });
-  klass.define_method("greet", [](VALUE self) {
-      auto rb_name = rb_ivar_get(self, rb_intern("@name"));
-      rb::Object message(rb_str_new_cstr("Hello "));
-      return static_cast<VALUE>(message.send("+", {rb_name}));
+  klass.define_method("greet", [](VALUE rb_self) {
+      rb::Object self(rb_self);
+      rb::Object prefix(rb_str_new_cstr("Hello "));
+      auto message = prefix.send("+", {self.ivar_get("@name")});
+      return static_cast<VALUE>(message);
     });
 }
 
