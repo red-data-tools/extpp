04a54d2e7c6b05f3a317643b45db6076a4632909
Kouhei Sutou <kou@clear-code.com>
2019-03-21 06:02:37 +0900

    windows: fix path

--- lib/extpp/setup.rb
+++ lib/extpp/setup.rb
@@ -1,4 +1,4 @@
 if Object.const_defined?(:RubyInstaller)
-  ext_dir = File.expand_path(File.join(__dir__, "..", "ext", "extpp"))
+  ext_dir = File.expand_path(File.join(__dir__, "..", "..", "ext", "extpp"))
   RubyInstaller::Runtime.add_dll_directory(ext_dir)
 end
