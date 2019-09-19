355efe4be483d65fde7094e09512995186c0cd96
Sutou Kouhei <kou@clear-code.com>
2019-09-20 06:11:49 +0900

    Use SOEXT
    Suggested by Nobuyoshi Nakada. Thanks!!!

--- lib/extpp/platform.rb
+++ lib/extpp/platform.rb
@@ -1,14 +1,7 @@
 module ExtPP
   class Platform
     def dynamic_library_extension
-      case RUBY_PLATFORM
-      when /windows/, /mingw/
-        "dll"
-      when /darwin/
-        "dylib"
-      else
-        RbConfig::CONFIG["DLEXT"]
-      end
+      RbConfig::CONFIG["SOEXT"]
     end
   end
 end
